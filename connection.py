5f57802266b7178753d73c9e75f9ca9891d46d57
InfinitiWarrior <79752294+InfinitiWarrior@users.noreply.github.com>
2024-10-17 09:03:46 +0200

    working database and stuff

--- connection.py
+++ connection.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sqlite3
 
 class PostDB:
-    def __init__(self, db_name='database.db'):
+    def __init__(self, db_name='db.sqlite3'):
         self.conn = sqlite3.connect(db_name)
         self.conn.row_factory = sqlite3.Row
         self.init_db()
@@ -12,7 +12,7 @@ class PostDB:
             CREATE TABLE IF NOT EXISTS posts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 title VARCHAR(200) NOT NULL,
-                content TEXT NOT NULL,
+                pages INT NOT NULL,
                 price INT NOT NULL,
                 amount INT NOT NULL
             )
@@ -22,14 +22,14 @@ class PostDB:
     def fetch_all(self):
         return self.conn.execute('SELECT * FROM posts').fetchall()
 
-    def add_post(self, title, content, price, amount):
-        self.conn.execute('INSERT INTO posts (title, content, price, amount) VALUES (?, ?, ?, ?)',
-                          (title, content, price, amount))
+    def add_post(self, title, pages, price, amount):
+        self.conn.execute('INSERT INTO posts (title, pages, price, amount) VALUES (?, ?, ?, ?)',
+                          (title, pages, price, amount))
         self.conn.commit()
 
-    def update_post(self, id, title, content, price, amount):
-        self.conn.execute('UPDATE posts SET title = ?, content = ?, price = ?, amount = ? WHERE id = ?',
-                          (title, content, price, amount, id))
+    def update_post(self, id, title, pages, price, amount):
+        self.conn.execute('UPDATE posts SET title = ?, pages = ?, price = ?, amount = ? WHERE id = ?',
+                          (title, pages, price, amount, id))
         self.conn.commit()
 
     def delete_post(self, id):
@@ -47,7 +47,7 @@ st.header('All Posts')
 posts = db.fetch_all()
 if posts:
     for post in posts:
-        st.write(f"ID: {post['id']}, Title: {post['title']}, Price: {post['price']}, Amount: {post['amount']}")
+        st.write(f"ID: {post['id']}, Title: {post['title']}, Pages: {post['pages']}, Price: {post['price']}, Amount: {post['amount']}")
 else:
     st.write("No posts available.")
 
@@ -55,13 +55,13 @@ else:
 st.header('Create New Post')
 with st.form('create_form'):
     title = st.text_input('Title')
-    content = st.text_area('Content')
+    pages = st.text_area('pages')
     price = st.number_input('Price', min_value=0)
     amount = st.number_input('Amount', min_value=0)
     submitted = st.form_submit_button('Create')
 
     if submitted:
-        db.add_post(title, content, price, amount)
+        db.add_post(title, pages, price, amount)
         st.success('Post created!')
 
 # Update a post
@@ -69,13 +69,13 @@ st.header('Update Post')
 with st.form('update_form'):
     post_id = st.number_input('Post ID', min_value=1)
     new_title = st.text_input('New Title')
-    new_content = st.text_area('New Content')
+    new_pages = st.text_area('New Pages')
     new_price = st.number_input('New Price', min_value=0)
     new_amount = st.number_input('New Amount', min_value=0)
     update_submitted = st.form_submit_button('Update')
 
     if update_submitted:
-        db.update_post(post_id, new_title, new_content, new_price, new_amount)
+        db.update_post(post_id, new_title, new_pages, new_price, new_amount)
         st.success('Post updated!')
 
 # Delete a post
