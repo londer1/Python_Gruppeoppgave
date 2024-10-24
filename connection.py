2dbd20eb45c7f7c8e7cff21eeb583eeca0f442db
Even <evenfbh@gmail.com>
2024-10-24 08:34:23 +0200

    Updated Search

--- connection.py
+++ connection.py
@@ -84,20 +84,5 @@ class UserDB:
         self.conn.execute('DELETE FROM Users WHERE id = ?', (id,))
         self.conn.commit()
 
-class Search():
-    def __init__(self, query, database='db.sqlite3'):
-        self.query = query.lower()
-        self.database = database
-        self.results = []
-        self.conn = sqlite3.connect(self.database)
-        self.cursor = self.conn.cursor()
-    def search(self):
-        if self.query:
-            sql_query = 'SELECT * FROM Posts WHERE title LIKE ? OR genre LIKE ?'
-            like_query = f'%{self.query}%'
-            self.cursor.execute(sql_query, (like_query, like_query))
-            self.results = self.cursor.fetchall()
-            return self.results
-    def close():
-        self.conn.close()
+
 
